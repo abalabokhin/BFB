54bab7d7c4b1653b4390aa990ba3fdb0ad920470
Andrey Balabokhin <abalabokhin@gmail.com>
2013-09-01 15:20:50 -0400

    created the project and added some objects to understand the process. Added all the file to the repository. Some of them can be temporary.

---
+++ Assets/MoveAround.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var speed = 10.0;
+var rotateSpeed = 10.0;
+
+function Update () {
+	var controller : CharacterController = GetComponent(CharacterController);
+	
+	transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+	
+	var forward = transform.TransformDirection(Vector3.forward);
+	var curSpeed = speed * Input.GetAxis("Vertical");
+	controller.SimpleMove(forward * curSpeed);
+
+}
+@script RequireComponent(CharacterController)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 BFB
 ===
+It is a great game about something. Now it is a ball that you can move on the plane with a cube.
+
+===
+How to run the project:
+1. Download and install Unity 3d.
+2. Open the project
+3. Choose first levelfrom "Assets" sub-window.
+4. Click "Run" button at the top of the main unity window.
