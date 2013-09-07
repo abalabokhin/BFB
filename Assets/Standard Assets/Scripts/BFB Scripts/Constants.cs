e0af6df5e30e7b80ce13b39926d04f32b9616cff
Andrey Balabokhin <abalabokhin@gmail.com>
2013-09-07 19:45:12 -0400

    added gravity, failed to added collision detector, removed temporary files from project

---
+++ Assets/Standard Assets/Scripts/BFB Scripts/Constants.cs
@@ -0,0 +1,6 @@
+using System;
+
+public class Constants
+{
+	public const float gravityCoefficient = 100.0f;
+}
--- Assets/Standard Assets/Scripts/BFB Scripts/MoveAround.cs
+++ Assets/Standard Assets/Scripts/BFB Scripts/MoveAround.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 public class MoveAround : MonoBehaviour
 {	
 	IList<GameObject> planets = new List<GameObject>();
-	float rotateForce = 1f;
+	//float rotateForce = 1f;
+	float rotateSpeed = 3f;
 	float moveForce = 3f;
 	
 	void Start ()
@@ -19,8 +20,27 @@ public class MoveAround : MonoBehaviour
 	{
 		float hInput = Input.GetAxis ("Horizontal");
 		float vInput = Input.GetAxis ("Vertical");
+	
+		gameObject.transform.Rotate(0, hInput * rotateSpeed, 0);
 		
-		gameObject.rigidbody.AddTorque (gameObject.transform.up * rotateForce * hInput, ForceMode.Force);
+		//gameObject.rigidbody.AddTorque (gameObject.transform.up * rotateForce * hInput, ForceMode.Force);
 		gameObject.rigidbody.AddForce (gameObject.transform.forward * moveForce * vInput, ForceMode.Force);
+		
+	
+		/// gravity force from all the planets.
+		Vector3 position = transform.position; 
+	
+		foreach (GameObject planet in planets) {
+			Vector3 direction = (planet.transform.position - position);
+			var distance = direction.magnitude; 
+			float planetMass = planet.GetComponent<Rigidbody>().mass;
+			float forceModule = Constants.gravityCoefficient * planetMass / (distance * distance);
+			gameObject.rigidbody.AddForce (direction * forceModule, ForceMode.Force);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("collided");
 	}
 }
