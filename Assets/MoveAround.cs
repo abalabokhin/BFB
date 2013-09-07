85bb5afddbb34f9e510605b4faddd759b29f1bf2
fuzzley <fuzz357@hotmail.com>
2013-09-07 09:45:03 -0400

    Added two different 3d models for spaceship. Need to get 3ds max or cinema 4d to use them or to export compatible versions for unity. Also replaced the js script with a C# script. Seems to be working okay.

---
+++ Assets/MoveAround.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAround : MonoBehaviour
+{	
+	float rotateForce = 1f;
+	float moveForce = 3f;
+	
+	void Start ()
+	{
+	}
+	
+	void Update ()
+	{
+		float hInput = Input.GetAxis ("Horizontal");
+		float vInput = Input.GetAxis ("Vertical");
+		
+		if (hInput > 0 || vInput > 0) 
+		{
+			Debug.Log (string.Format ("hInput: {0}\nvInput: {1}", hInput, vInput));
+		}
+		
+		gameObject.rigidbody.AddTorque (gameObject.transform.up * rotateForce * hInput, ForceMode.Force);
+		gameObject.rigidbody.AddForce (gameObject.transform.forward * moveForce * vInput, ForceMode.Force);
+	}
+}
---
+++ Assets/MoveAroundjs.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+//var planets;
+//
+//function start () {
+//	if (planets == null) {
+//		planets	= gameobject.findgameobjectswithtag ("planet");
+//	}
+//}
+//
+//var speed = 10.0;
+//var rotatespeed = 10.0;
+//
+//function update () {
+//	var controller : charactercontroller = getcomponent(charactercontroller);
+//	
+//	transform.rotate(0, input.getaxis("horizontal") * rotatespeed, 0);
+//	
+//	var forward = transform.transformdirection(vector3.forward);
+//	var curspeed = speed * input.getaxis("vertical");
+//	controller.simplemove(forward * curspeed);
+//
+//	var position = transform.position; 
+//	for (var planet : gameobject in planets) {
+//		var diff = (planet.transform.position - position);
+//		var curdistance = diff.sqrmagnitude; 
+//		debug.log(curdistance);
+//	}
+//
+//}
+//@script requirecomponent(charactercontroller)
