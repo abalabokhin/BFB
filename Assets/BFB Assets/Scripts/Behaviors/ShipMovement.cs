ed006c83355d75f3428009e90e0ef4f53677d9e7
fuzzley <fuzz357@hotmail.com>
2013-09-22 17:31:38 -0400

    Added a new spaceship and playing around with BFB level.

--- Assets/BFB Assets/Scripts/Behaviors/ShipMovement.cs
+++ Assets/BFB Assets/Scripts/Behaviors/ShipMovement.cs
@@ -10,9 +10,14 @@ public class ShipMovement : MonoBehaviour
 {
     public float moveForce = 1000f;
     public float rotateSpeed = 100f;
+    public GameObject target;
 
     void Start()
     {
+        if (target == null)
+        {
+            target = gameObject;
+        }
         SetFlamesEnabled(false);
     }
 
@@ -21,18 +26,18 @@ public class ShipMovement : MonoBehaviour
         // show menu if 'esc' key pressed.
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            gameObject.GetComponent<GameMenu>().enabled = true;
+            target.GetComponent<GameMenu>().enabled = true;
             SessionCache.Cache.LevelInspector.currentState = LevelInspector.GameState.Pause;
         }
 
         float hInput = Input.GetAxis("Horizontal");
         float vInput = Input.GetAxis("Vertical");
 
-        gameObject.transform.Rotate(0, hInput * rotateSpeed * Time.deltaTime, 0);
-        //gameObject.rigidbody.AddTorque (0, hInput * rotateForce, 0);
+        target.transform.Rotate(0, hInput * rotateSpeed * Time.deltaTime, 0);
+        //target.rigidbody.AddTorque (0, hInput * rotateForce, 0);
 
-        Vector3 forwardForce = gameObject.transform.forward * moveForce * vInput * Time.deltaTime;
-        gameObject.rigidbody.AddForce(forwardForce, ForceMode.Force);
+        Vector3 forwardForce = target.transform.forward * moveForce * vInput * Time.deltaTime;
+        target.rigidbody.AddForce(forwardForce, ForceMode.Force);
         if (forwardForce.magnitude > 0)
         {
             SetFlamesEnabled(true);
@@ -51,7 +56,7 @@ public class ShipMovement : MonoBehaviour
                 direction.Normalize();
                 float planetMass = planetObject.GetComponent<PlanetBehaviour>().mass;
                 float forceModule = Constants.gravityCoefficient * planetMass / (distance * distance);
-                gameObject.rigidbody.AddForce(direction * forceModule, ForceMode.Force);
+                target.rigidbody.AddForce(direction * forceModule, ForceMode.Force);
             }
         }
     }
@@ -60,10 +65,10 @@ public class ShipMovement : MonoBehaviour
     {
         Debug.Log("collided with " + other.tag);
 		if (other.CompareTag(Tags.planet)) {
-        	gameObject.GetComponent<GameMenu>().enabled = true;
+        	target.GetComponent<GameMenu>().enabled = true;
         	SessionCache.Cache.LevelInspector.currentState = LevelInspector.GameState.Destroyed;
 		} else if (other.CompareTag(Tags.winPoint)) {
-			gameObject.GetComponent<GameMenu>().enabled = true;
+			target.GetComponent<GameMenu>().enabled = true;
 			SessionCache.Cache.LevelInspector.NextLevel();
 		}
     }
@@ -72,7 +77,7 @@ public class ShipMovement : MonoBehaviour
     {
         //IList<GameObject> flameParticleSystems = new List<GameObject>();
         //IList<GameObject> flames = GameObject.FindGameObjectsWithTag (Tags.flame);
-        //ParticleSystem particle = gameObject.GetComponent (typeof(ParticleSystem)) as ParticleSystem;
+        //ParticleSystem particle = target.GetComponent (typeof(ParticleSystem)) as ParticleSystem;
         //GameObject particlesystem = flames[0];
         //if (particlesystem != null) {
         //	Debug.Log ("Here");
