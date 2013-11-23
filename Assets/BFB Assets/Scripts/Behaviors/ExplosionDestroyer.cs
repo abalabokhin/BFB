daa3e151d7ef1bf605ddbc206245a602e07aef2c
Andrey Balabokhin <abalabokhin@gmail.com>
2013-11-23 16:10:04 -0500

    added explosion of asteroids when destroyed

---
+++ Assets/BFB Assets/Scripts/Behaviors/ExplosionDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void DestroyObject(GameObject collidedObject)
+    {
+		/// make asteroid invisible
+		gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+		gameObject.GetComponent<Detonator>().enabled = true;
+        Destroy(gameObject, 2f);
+    }
+}
